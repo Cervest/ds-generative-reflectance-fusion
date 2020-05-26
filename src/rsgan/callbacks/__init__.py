857a4da9596d646eb6fcc0414ce70dad857c8e7e
shahine <shahine96@gmail.com>
2020-05-26 13:58:39 +0200

    added CALLBACKS + factory

---
+++ src/rsgan/callbacks/__init__.py
@@ -0,0 +1,24 @@
+from src.utils import Registry
+"""
+Registery of common callbacks
+"""
+CALLBACKS = Registry()
+
+
+def build_callback(cfg):
+    experiment = CALLBACKS[cfg['name']](cfg)
+    return experiment
+
+
+@CALLBACKS.register_fn('early_stopping')
+def build_early_stopping(cfg):
+    """Builds early stopping callback
+    """
+    if cfg:
+        import pytorch_lightning as pl
+        params = {'monitor': cfg['monitor'],
+                  'patience': cfg['patience']}
+        callback = pl.callbacks.EarlyStopping(**params)
+    else:
+        callback = False
+    return callback
