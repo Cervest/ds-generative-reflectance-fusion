6ba4c71efcca46b914ca303ef3bbdf31f43fe1e7
shahine <shahine96@gmail.com>
2020-05-26 18:15:20 +0200

    modified DummyCloudRemovalDataset to load from multiple views

--- src/rsgan/data/datasets/cloud_removal_toy.py
+++ src/rsgan/data/datasets/cloud_removal_toy.py
@@ -1,4 +1,6 @@
 import os
+from operator import add
+from functools import reduce
 from torch.utils.data import Dataset
 from src.toygeneration import ProductDataset
 from src.rsgan.data import DATASETS
@@ -15,10 +17,43 @@ class DummyCloudRemovalDataset(Dataset):
         raw_sar_root (str): path to toy sar images dataset
         enhanced_optical_root (str): path to toy clean optical images dataset
     """
-    def __init__(self, raw_optical_root, raw_sar_root, enhanced_optical_root):
-        self._raw_optical_dataset = ProductDataset(raw_optical_root)
-        self._raw_sar_dataset = ProductDataset(raw_sar_root)
-        self._enhanced_optical_dataset = ProductDataset(enhanced_optical_root)
+    _raw_optical_dirname = "clouded_optical"
+    _raw_sar_dirname = "sar"
+    _clean_optical_dirname = "clean_optical"
+
+    def __init__(self, root):
+        root = "../data/toy/dummy_cloud_removal/"
+        buffer = self._load_datasets(root)
+        self._raw_optical_dataset = buffer[0]
+        self._raw_sar_dataset = buffer[1]
+        self._enhanced_optical_dataset = buffer[2]
+
+    def _load_datasets(self, root):
+        """Loads and concatenates datasets from multiple views of raw optical,
+        raw sar and enhanced optical imagery
+
+        Args:
+            root (str)
+
+        Returns:
+            type: tuple[ProductDataset]
+        """
+        # Init empy dataset lists
+        raw_optical_dataset = []
+        raw_sar_dataset = []
+        enhanced_optical_dataset = []
+
+        # Fill with datasets from each individual views
+        for seed in os.listdir(root):
+            raw_optical_dataset += [ProductDataset(os.path.join(root, seed, self._raw_optical_dirname))]
+            raw_sar_dataset += [ProductDataset(os.path.join(root, seed, self._raw_sar_dirname))]
+            enhanced_optical_dataset += [ProductDataset(os.path.join(root, seed, self._clean_optical_dirname))]
+
+        # Concatenate into single datasets
+        raw_optical_dataset = reduce(add, raw_optical_dataset)
+        raw_sar_dataset = reduce(add, raw_sar_dataset)
+        enhanced_optical_dataset = reduce(add, enhanced_optical_dataset)
+        return raw_optical_dataset, raw_sar_dataset, enhanced_optical_dataset
 
     def __getitem__(self, index):
         raw_optical, _ = self.raw_optical_dataset[index]
@@ -43,8 +78,4 @@ class DummyCloudRemovalDataset(Dataset):
 
     @classmethod
     def build(cls, cfg):
-        root = cfg['root']
-        dataset_kwargs = {'raw_optical_root': os.path.join(root, "clouded_optical"),
-                          'raw_sar_root': os.path.join(root, "sar"),
-                          'enhanced_optical_root': os.path.join(root, "clean_optical")}
-        return cls(**dataset_kwargs)
+        return cls(root=cfg['root'])
