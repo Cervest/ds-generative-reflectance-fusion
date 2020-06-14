fixed tangential scale distortion computation

src/toygeneration/modules/transforms/transforms.py
@@ -78,7 +78,7 @@ class TangentialScaleDistortion(iaa.Augmenter):
         image_size (tuple[int]): (width, height)
         mesh_size (tuple[int]): (n_cells_columns, n_cells_rows) number of mesh cells in
             rows and columns for piecewise affine morphing
-        axis (int): distortion axis {width/rows: 1, height/columns: 0}
+        axis (int): distortion axis {height/rows: 0, width/columns: 1}
         growth_rate (float): sigmoid growth rate parameter
             (default : 4 / length_distortion_axis)
     """
@@ -94,16 +94,16 @@ class TangentialScaleDistortion(iaa.Augmenter):
     def _build_source_meshgrid(self, image_size, mesh_size):
         """Creates meshgrids of image size and number of mesh specified
         Args:
-            image_size (tuple[int]): (width, height)
-            mesh_size (tuple[int]): (n_cells_columns, n_cells_rows) number of mesh cells in
+            image_size (tuple[int]): (height, width)
+            mesh_size (tuple[int]): (n_cells_rows, n_cells_columns) number of mesh cells in
                 rows and columns for piecewise affine morphing
         Returns:
             type: np.ndarray
         """
         # Build source meshgrid
-        w, h = image_size
-        src_cols = np.linspace(0, w, mesh_size[0])
-        src_rows = np.linspace(0, h, mesh_size[1])
+        h, w = image_size
+        src_rows = np.linspace(0, h, mesh_size[0])
+        src_cols = np.linspace(0, w, mesh_size[1])
         src_rows, src_cols = np.meshgrid(src_rows, src_cols)
         src = np.dstack([src_cols.flat, src_rows.flat])[0]
         return src
@@ -114,18 +114,17 @@ class TangentialScaleDistortion(iaa.Augmenter):
 
         Args:
             src (np.ndarray): source meshgrid
-            axis (int): distortion axis {width/rows: 1, height/columns: 0}
+            axis (int): distortion axis {height/rows: 0, width/columns: 1}
 
         Returns:
             type: np.ndarray
         """
         # Apply deformation on specified axis to obtain target meshgrid
-        if axis == 1:
+        if axis == 0:
             tgt_rows = self._deform_axis(src[:, 1])
             tgt_cols = src[:, 0]
-            tgt = np.vstack([tgt_cols, tgt_rows]).T
 
-        elif axis == 0:
+        elif axis == 1:
             tgt_rows = src[:, 1]
             tgt_cols = self._deform_axis(src[:, 0])
 