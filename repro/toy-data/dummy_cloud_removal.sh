f53bcaa7293a8ab66f485e2bc6c31a45c1db4694
shahine <shahine96@gmail.com>
2020-06-14 21:37:36 +0200

    update repro bash script

--- repro/toy-data/dummy_cloud_removal.sh
+++ repro/toy-data/dummy_cloud_removal.sh
@@ -4,17 +4,22 @@ for seed in {1..50}
 do
   printf "\n GENERATING FOR SEED = "$seed"\n"
   # Setup dump directories paths
+  latent_optical_path=$data_root$seed"/latent_optical"
   clean_optical_path=$data_root$seed"/clean_optical"
-  clean_sar_path=$data_root$seed"/clean_sar"
+  latent_sar_path=$data_root$seed"/latent_sar"
   clouded_optical_path=$data_root$seed"/clouded_optical"
   derived_sar_path=$data_root$seed"/sar"
-  # Run toy product generation and derivation
-  python run_toy_generation.py --cfg=$config_root"optical/generation_clean_optical.yaml" \
-  --o=$clean_optical_path --seed=$seed
+  # Run toy optical product generation
+  python run_toy_generation.py --cfg=$config_root"optical/generation_latent_optical.yaml" \
+  --o=$latent_optical_path --seed=$seed
+  python run_toy_derivation.py --cfg=$config_root"optical/derivation_clean_optical.yaml" \
+  --o=$clean_optical_path
   python run_toy_derivation.py --cfg=$config_root"optical/derivation_clouded_optical.yaml" \
   --o=$clouded_optical_path --product=$clean_optical_path
-  python run_toy_generation.py --cfg=$config_root"sar/generation_clean_sar.yaml" \
-  --o=$clean_sar_path --seed=$seed
+  rm -rf $latent_optical_path
+  # Run toy SAR product generation
+  python run_toy_generation.py --cfg=$config_root"sar/generation_latent_sar.yaml" \
+  --o=$latent_sar_path --seed=$seed
   python run_toy_derivation.py --cfg=$config_root"sar/derivation_sar.yaml" \
   --o=$derived_sar_path --product=$clean_sar_path
   rm -rf $clean_sar_path
