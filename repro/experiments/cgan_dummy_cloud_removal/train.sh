d970ca5cf718c7554d776a95a387561abc73c26b
shahineb <shahine96@gmail.com>
2020-06-15 09:10:25 +0100

    destroyed and reinited dvc pipeline

--- repro/experiments/cgan_dummy_cloud_removal/train.sh
+++ repro/experiments/cgan_dummy_cloud_removal/train.sh
@@ -1,6 +1,5 @@
-dvc run -v -f repro/downloads/cgan_dummy_cloud_removal/train.dvc \
+dvc run -v -f repro/experiments/cgan_dummy_cloud_removal/train.dvc \
 -d src/rsgan/experiments/cgan_cloud_removal.py \
 -d src/rsgan/config/cgan_dummy_cloud_removal.yaml \
 -o data/experiments_outputs/ \
-python run_training.py --cfg=src/rsgan/config/cgan_dummy_cloud_removal.yaml \
---o=data/experiments_outputs/cgan_dummy_cloud_removal --device=0 --name=dvc_run
+"python run_training.py --cfg=src/rsgan/config/cgan_dummy_cloud_removal.yaml --o=data/experiments_outputs/cgan_dummy_cloud_removal --device=0 --experiment_name=dvc_run"
