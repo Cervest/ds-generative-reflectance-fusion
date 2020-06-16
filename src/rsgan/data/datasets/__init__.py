4a7be3e7f3e1a6cb579cb71276590ab9bb81aa78
shahine <shahine96@gmail.com>
2020-06-16 15:33:07 +0200

    Added SARToOpticalDataset for toy data class

--- src/rsgan/data/datasets/__init__.py
+++ src/rsgan/data/datasets/__init__.py
@@ -1,3 +1,4 @@
 from .dummy_cloud_removal import DummyCloudRemovalDataset
+from .dummy_sar_to_optical import DummySARToOptical
 
-__all__ = ['DummyCloudRemovalDataset']
+__all__ = ['DummyCloudRemovalDataset', 'DummySARToOptical']
