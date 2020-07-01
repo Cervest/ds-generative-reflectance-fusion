328104ebd74bd827f10d6dfe58fde20d66400865
shahineb <shahine96@gmail.com>
2020-07-01 16:00:17 +0100

    added datasets relative import modules

--- src/rsgan/data/datasets/__init__.py
+++ src/rsgan/data/datasets/__init__.py
@@ -1,4 +1,3 @@
-from .dummy_cloud_removal import DummyCloudRemovalDataset
-from .dummy_sar_to_optical import DummySARToOptical
+from .toy import ToyCloudRemovalDataset, ToySARToOptical
 
-__all__ = ['DummyCloudRemovalDataset', 'DummySARToOptical']
+__all__ = ['ToyCloudRemovalDataset', 'ToySARToOptical']
