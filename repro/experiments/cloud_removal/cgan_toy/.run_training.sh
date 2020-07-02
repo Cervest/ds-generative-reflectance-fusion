ff1b239715da3e7361919f4a5fbb6f29f9ad381c
shahineb <shahine96@gmail.com>
2020-07-02 12:11:16 +0100

    restructured repro directory

---
+++ repro/experiments/cloud_removal/cgan_toy/.run_training.sh
@@ -0,0 +1,20 @@
+# Retrive device id
+for i in "$@"
+do
+case $i in
+    --device=*)
+    DEVICE="${i#*=}"
+    shift # past argument=value
+    ;;
+    *)
+          # unknown option
+    ;;
+esac
+done
+
+# Run dvc pipeline on specified device
+dvc run -v -f repro/experiments/cloud_removal/cgan_toy/run_training.dvc \
+-d src/rsgan/experiments/cloud_removal/cgan_toy_cloud_removal.py \
+-d src/rsgan/config/cloud_removal/cgan_toy.yaml \
+-o data/experiments_outputs/cgan_toy_cloud_removal/dvc_run/run/ \
+"python run_training.py --cfg=src/rsgan/config/cloud_removal/cgan_toy.yaml --o=data/experiments_outputs/cgan_toy_cloud_removal --device=$DEVICE --experiment_name=dvc_run"
