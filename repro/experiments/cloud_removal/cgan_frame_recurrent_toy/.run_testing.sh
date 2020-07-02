5bda092c657d03c1fdcfe978374db337d41dff24
shahineb <shahine96@gmail.com>
2020-07-02 10:30:50 +0100

    update repro files for experiments

---
+++ repro/experiments/cloud_removal/cgan_frame_recurrent_toy/.run_testing.sh
@@ -0,0 +1,21 @@
+# Retrive device id
+for i in "$@"
+do
+case $i in
+    --device=*)
+    DEVICE="${i#*=}"
+    shift # past argument=value
+    ;;
+    *)
+          # unknown option
+    ;;
+esac
+done
+
+# Run dvc pipeline on specified device
+dvc run -v -f repro/experiments/cloud_removal/cgan_frame_recurrent_toy/run_testing.dvc \
+-d src/rsgan/experiments/cloud_removal/cgan_frame_recurrent_toy_cloud_removal.py \
+-d src/rsgan/config/cloud_removal/cgan_frame_recurrent_toy.yaml \
+-o data/experiments_outputs/cgan_frame_recurrent_toy_cloud_removal/dvc_run/eval/ \
+"python make_baseline_classifier.py --cfg=src/rsgan/config/cloud_removal/cgan_frame_recurrent_toy.yaml --o=data/experiments_outputs/cgan_frame_recurrent_toy_cloud_removal/dvc_run/eval/baseline_classifier/classifier.pickle \
+&& python run_testing.py --cfg=src/rsgan/config/cloud_removal/cgan_frame_recurrent_toy.yaml --o=data/experiments_outputs/cgan_frame_recurrent_toy_cloud_removal --device=$DEVICE"
