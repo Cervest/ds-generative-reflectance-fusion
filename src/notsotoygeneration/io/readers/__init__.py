3ed6eb576c2e732529f4a253c0d4db519499f9d5
shahine <shahine96@gmail.com>
2020-07-29 16:27:36 +0200

    added Landsat scene writer and reader

--- src/notsotoygeneration/io/readers/__init__.py
+++ src/notsotoygeneration/io/readers/__init__.py
@@ -1,6 +1,7 @@
 from .s2_reader import S2BandReader, S2SceneReader
 from .s1_reader import S1BandReader, S1SceneReader
 from .modis_reader import MODISBandReader, MODISSceneReader
+from .landsat_reader import LandsatBandReader, LandasatSceneReader
 
-__all__ = ['S2BandReader', 'S1BandReader', 'MODISBandReader',
-           'S2SceneReader', 'S1SceneReader', 'MODISSceneReader']
+__all__ = ['S2BandReader', 'S1BandReader', 'MODISBandReader', 'LandsatBandReader',
+           'S2SceneReader', 'S1SceneReader', 'MODISSceneReader', 'LandasatSceneReader']
