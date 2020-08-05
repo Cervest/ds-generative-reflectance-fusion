3343389524d6010dd0112ce0713c4efa1811f6da
shahine <shahine96@gmail.com>
2020-08-05 17:13:37 +0100

    addeed multihead unet

--- src/rsgan/experiments/modis_landsat_fusion/__init__.py
+++ src/rsgan/experiments/modis_landsat_fusion/__init__.py
@@ -1,7 +1,9 @@
 from .unet_modis_landsat_fusion import UNetMODISLandsatTemporalResolutionFusion
 from .resnet_modis_landsat_fusion import ResnetMODISLandsatTemporalResolutionFusion
 from .late_fusion_resnet_modis_landsat_fusion import LateFusionResnetMODISLandsatTemporalResolutionFusion
+from .multi_head_unet_modis_landsat_fusion import MultiHeadUNetMODISLandsatTemporalResolutionFusion
 
 __all__ = ['UNetMODISLandsatTemporalResolutionFusion',
            'ResnetMODISLandsatTemporalResolutionFusion',
-           'LateFusionResnetMODISLandsatTemporalResolutionFusion']
+           'LateFusionResnetMODISLandsatTemporalResolutionFusion',
+           'MultiHeadUNetMODISLandsatTemporalResolutionFusion']
