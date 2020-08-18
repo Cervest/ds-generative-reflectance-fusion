90e4fca6eb3bee108ed7ceb095058d8040f7267a
shahine <shahine96@gmail.com>
2020-08-18 11:39:30 +0100

    push to tardis

--- src/rsgan/evaluation/ESTARFM.py
+++ src/rsgan/evaluation/ESTARFM.py
@@ -42,7 +42,7 @@ def main(args):
             predicted_bands = load_in_multiband_raster(files_paths)
 
             # Load groundtruth frame
-            target_directory = os.path.join(args['--target'], patch_idx, date, 'landsat')
+            target_directory = os.path.join(args['--target'], patch_idx, 'landsat', date)
             target_files_paths = [os.path.join(date_directory, band) for band in os.listdir(target_directory)]
             target_bands = load_in_multiband_raster(target_files_paths)
 
