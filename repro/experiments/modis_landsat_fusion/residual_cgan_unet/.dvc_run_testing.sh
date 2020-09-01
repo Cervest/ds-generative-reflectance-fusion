de83cd373d4e05c8194310fce6d29286f2794423
shahine <shahine96@gmail.com>
2020-09-01 10:29:59 +0100

    upadted repros

--- repro/experiments/modis_landsat_fusion/residual_cgan_unet/.dvc_run_testing.sh
+++ repro/experiments/modis_landsat_fusion/residual_cgan_unet/.dvc_run_testing.sh
@@ -17,11 +17,19 @@ CONFIG=src/deep_reflectance_fusion/config/modis_landsat_fusion/generative/residu
 EXPERIMENT=src/deep_reflectance_fusion/experiments/modis_landsat_fusion/cgan_fusion_modis_landsat.py
 DATASET=data/patches/modis_landsat
 ROOT=data/experiments_outputs/modis_landsat_fusion/residual_cgan_unet
+SEEDS=(17 37 43 73 101)
+CHKPTS=("seed_17/checkpoints/epoch=42.ckpt"
+        "seed_37/checkpoints/epoch=62.ckpt"
+        "seed_43/checkpoints/epoch=60.ckpt"
+        "seed_73/checkpoints/epoch=63.ckpt"
+        "seed_101/checkpoints/epoch=61.ckpt")
+
 
 # Run dvc pipeline on specified device
-for SEED in {17, 37, 43, 73, 101}:
+for (( i=0; i<${#SEEDS[*]}; ++i));
 do
-  NAME=seed_$SEED
+  NAME=seed_${SEEDS[$i]}
+  CHKPT=$ROOT/${CHKPTS[$i]}
   TRAIN_DIR=$ROOT/$NAME/run
   TEST_DIR=$ROOT/$NAME/eval
   dvc run -v -f -n test_modis_landsat_fusion_residual_cgan_unet_$NAME \
@@ -30,5 +38,5 @@ do
   -d $DATASET \
   -d $TRAIN_DIR \
   -o $TEST_DIR \
-  "python run_testing.py --cfg=$CONFIG --o=$ROOT --device=$DEVICE"
+  "python run_testing.py --cfg=$CONFIG --o=$ROOT --device=$DEVICE --chkpt=$CHKPT"
 done
--- repro/experiments/modis_landsat_fusion/residual_cgan_unet/.dvc_run_training.sh
+++ repro/experiments/modis_landsat_fusion/residual_cgan_unet/.dvc_run_training.sh
@@ -20,7 +20,7 @@ ROOT=data/experiments_outputs/modis_landsat_fusion/residual_cgan_unet
 
 
 # Run dvc pipeline on specified device
-for SEED in {17, 37, 43, 73, 101}:
+for SEED in 17 37 43 73 101 ;
 do
   NAME=seed_$SEED
   TRAIN_DIR=$ROOT/$NAME/run
@@ -29,5 +29,5 @@ do
   -d $EXPERIMENT \
   -d $DATASET \
   -o $TRAIN_DIR \
-  "python run_training.py --cfg=$CONFIG --o=$ROOT --device=$DEVICE --experiment_name=$NAME"
+  "python run_training.py --cfg=$CONFIG --o=$ROOT --device=$DEVICE --experiment_name=$NAME --seed==$SEED"
 done
