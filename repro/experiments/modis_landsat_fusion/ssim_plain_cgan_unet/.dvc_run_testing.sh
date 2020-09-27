acfcba2cef4160bb9ef246e7ed7ee3f48ff8a0af
shahine <shahine96@gmail.com>
2020-09-27 15:34:42 +0200

    added repro file for ssim

---
+++ repro/experiments/modis_landsat_fusion/ssim_plain_cgan_unet/.dvc_run_testing.sh
@@ -0,0 +1,42 @@
+# Retrive device id
+for i in "$@"
+do
+case $i in
+    --device=*)
+    DEVICE="${i#*=}"
+    shift # past argument=value
+    ;;
+    *)
+          # unknown option
+    ;;
+esac
+done
+
+# Define main path variables
+CONFIG=src/deep_reflectance_fusion/config/modis_landsat_fusion/generative/ssim_cgan_fusion_unet.yaml
+EXPERIMENT=src/deep_reflectance_fusion/experiments/modis_landsat_fusion/cgan_fusion_modis_landsat.py
+DATASET=data/patches/modis_landsat
+ROOT=data/experiments_outputs/modis_landsat_fusion/ssim_plain_cgan_unet
+SEEDS=(17 37 43 73 101)
+CHKPTS=("seed_17/checkpoints/epoch=284.ckpt"
+        "seed_37/checkpoints/epoch=324.ckpt"
+        "seed_43/checkpoints/epoch=275.ckpt"
+        "seed_73/checkpoints/epoch=275.ckpt"
+        "seed_101/checkpoints/epoch=275.ckpt")
+
+
+# Run dvc pipeline on specified device
+for (( i=0; i<${#SEEDS[*]}; ++i));
+do
+  NAME=seed_${SEEDS[$i]}
+  CHKPT=$ROOT/${CHKPTS[$i]}
+  TRAIN_DIR=$ROOT/$NAME/run
+  TEST_DIR=$ROOT/$NAME/eval
+  dvc run -v -f -n test_modis_landsat_fusion_ssim_plain_cgan_unet_$NAME \
+  -d $CONFIG \
+  -d $EXPERIMENT \
+  -d $DATASET \
+  -d $TRAIN_DIR \
+  -o $TEST_DIR \
+  "python run_testing.py --cfg=$CONFIG --o=$ROOT --device=$DEVICE --chkpt=$CHKPT"
+done
---
+++ repro/experiments/modis_landsat_fusion/ssim_plain_cgan_unet/.dvc_run_training.sh
@@ -0,0 +1,34 @@
+# Retrive device id
+for i in "$@"
+do
+case $i in
+    --device=*)
+    DEVICE="${i#*=}"
+    shift # past argument=value
+    ;;
+    *)
+          # unknown option
+    ;;
+esac
+done
+
+# Define main path variables
+CONFIG=src/deep_reflectance_fusion/config/modis_landsat_fusion/generative/ssim_cgan_fusion_unet.yaml
+EXPERIMENT=src/deep_reflectance_fusion/experiments/modis_landsat_fusion/cgan_fusion_modis_landsat.py
+DATASET=data/patches/modis_landsat
+ROOT=data/experiments_outputs/modis_landsat_fusion/ssim_plain_cgan_unet
+
+
+# Run dvc pipeline on specified device
+#for SEED in 17 37 43 73 101 ;
+for SEED in 73 ;
+do
+  NAME=seed_$SEED
+  TRAIN_DIR=$ROOT/$NAME/run
+  dvc run -v -f -n train_modis_landsat_fusion_ssim_plain_cgan_unet_$NAME \
+  -d $CONFIG \
+  -d $EXPERIMENT \
+  -d $DATASET \
+  -o $TRAIN_DIR \
+  "python run_training.py --cfg=$CONFIG --o=$ROOT --device=$DEVICE --experiment_name=$NAME"
+done
